98a9aca925a7826e7b42bf0a0f5674bb5e49b2bc
Chelsea Shimirwa <cshimirw@c1r3s5.42wolfsburg.de>
2025-09-11 13:41:52 +0200

    C00 Exercises ex00 to ex04) git push

---
+++ C00/ex03/ft_print_numbers.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cshimirw <cshimirw@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/10 15:17:29 by cshimirw          #+#    #+#             */
+/*   Updated: 2025/09/11 10:12:55 by cshimirw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_print_numbers(void)
+{
+    char number;
+    number = '0';
+    while (number <= '9')
+    {
+        write (1, &number, 1);
+        number++;
+    }
+}
+/*int main(void)
+{
+    ft_print_numbers();
+}*/
